3f26cf359e291b4201837c3fa6adc1dbbf207b2a
Antonio Carvalho <antoniorcn@hotmail.com>
2023-11-09 13:55:04 -0300

    PDMI

---
+++ pdmi/aula13/appav1/app/src/main/java/com/example/appav1/Cafe.java
@@ -0,0 +1,29 @@
+package com.example.appav1;
+
+public class Cafe {
+
+    private String nome;
+    private String base;
+    private double preco;
+
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getBase() {
+        return base;
+    }
+    public void setBase(String base) {
+        this.base = base;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
